0b8881b1371fefe60ead2a88749ba117bafb8517
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-12-08 20:04:00 +0200

    f

---
+++ Laba 13.1/dod.cpp
@@ -0,0 +1,6 @@
+﻿#include "dod.h"
+#include "var.h" // підключили зовнішні оголошення змінних
+using namespace nsVar;
+void nsDod::dod() {
+	a *= x * ((2.0 * n - 1.0) / (2.0 * n + 1.0)); // доданок домножується на 
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba 13.1/sum.h"
+#include "../Laba 13.1/Sum.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int x_p = 5; 
+			int x_k = 8;
+			int dx = 3; 
+			int e = 5;
+			Sum();
+		}
+	};
+}
